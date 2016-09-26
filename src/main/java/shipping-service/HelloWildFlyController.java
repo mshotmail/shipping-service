7c2d9f9b0d0c9a1d22b8a23237fc6cf54b676a65
mshotmail <msiraj@hotmail.com>
2016-09-26 14:13:10 -0400

    new

--- src/main/java/shipping-service/HelloWildFlyController.java
+++ src/main/java/shipping-service/HelloWildFlyController.java
@@ -1,4 +1,4 @@
-package orderservice;
+package shippingservice;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWildFlyController {
 
 
-    @RequestMapping("hello")
-    public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
+    @RequestMapping("ship")
+    public String sayShip(){
+        return ("Hello, from shippingservice");
     }
 }
